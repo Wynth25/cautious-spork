30b29ba16352d60014ca094f47227dc94ba2d0b8
Wynth25 <wynth25@gmail.com>
2026-03-23 18:31:07 +0100

    Add files via upload

---
+++ energy-arbitrage/solution.py
@@ -0,0 +1,42 @@
+#   <Task>
+
+import random
+prices = []
+prices = random.choices (range(1,101), k=24)
+for i in range(0, len(prices)):
+    print(f"{i}:00 - {i}:59 – {prices[i]}") 
+
+#   </Task>
+
+def calculate_max_profit(prices):
+    method = []
+    b = 0
+    profit = 0
+
+    for i in range(len(prices)):
+        if i == len(prices)-1:
+            if b == 0:
+                method.append("HOLD")
+            else:
+                method.append("SELL")
+                profit += prices[i]
+        
+        elif prices[i] > prices[i+1] and b == 1:
+            method.append("SELL")
+            b-=1
+            profit += prices[i]
+        
+        elif prices[i] < prices[i+1] and b == 0:
+            method.append("BUY")
+            b+=1
+            profit -= prices[i]
+        
+        else:
+            method.append("HOLD")
+
+    print(f"The maximum profit is: {profit}")
+    print(method)
+    
+
+
+calculate_max_profit(prices)
