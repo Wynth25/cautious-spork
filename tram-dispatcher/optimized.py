17b3c93c01b596627f1f6df76ae4feddfb549393
Wynth25 <wynth25@gmail.com>
2026-03-23 19:14:23 +0100

    Add files via upload

---
+++ tram-dispatcher/optimized.py
@@ -0,0 +1,16 @@
+#   <Task>
+import random
+schedule = []
+for _ in range(1, 100):
+	arrival = random.randint(0,1439)
+	departure = random.randint(arrival+1,1440)
+	schedule.append([arrival, departure])
+#   </Task>
+
+occ = [0]*1440
+for train in schedule:
+    for minute in range(train[0], train[1]):
+        occ[minute] += 1
+
+print(f"The required number of {max(occ)}.")
+print(f"Peak minute first appears  at minute {occ.index(max(occ))}.")
